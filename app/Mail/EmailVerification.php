000551c97ae7f35c744d4c3c7728073d3e4dd1b6
tanto <tantows001@gmail.com>
2024-03-11 10:14:50 +0700

    feat: user auth & email verification

---
+++ app/Mail/EmailVerification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class EmailVerification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $verificationUrl;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($verificationUrl)
+    {
+        $this->verificationUrl = $verificationUrl;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Email Verification',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Email Verification')
+                    ->view('emails.email_verification', [
+                        'verificationUrl'=>$this->verificationUrl
+                    ]);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url){
+            $currUrl =  $url;
+
+            return (new MailMessage)
+                ->subject('Verifikasi email')
+                ->line('Klik tombol dibawah untuk memverifikasi email')
+                ->action('Verifikasi email', $currUrl);
+        });
     }
 }
