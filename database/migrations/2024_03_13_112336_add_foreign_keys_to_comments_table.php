a697864df81adb17f1c48b8dfecafc7bea747e0b
LENOVO <annisaurohmah@mail.ugm.ac.id>
2024-03-13 11:59:17 +0700

    tables si portofolio ta

---
+++ database/migrations/2024_03_13_112336_add_foreign_keys_to_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->foreign(['id_content'])->references(['id'])->on('contents')->onUpdate('no action')->onDelete('cascade');
+            $table->foreign(['id_user'])->references(['id'])->on('users')->onUpdate('no action')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign('comments_id_content_foreign');
+            $table->dropForeign('comments_id_user_foreign');
+        });
+    }
+};
---
+++ database/migrations/2024_03_13_113313_create_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_user')->index();
+            $table->unsignedBigInteger('id_content')->index();
+            $table->text('notification_message');
+            $table->dateTime('notification_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
