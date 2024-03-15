593281cdbca3127ff9287a315b366c64c9c7fdae
Tantowi Shah <114087993+tantowish@users.noreply.github.com>
2024-03-15 10:28:14 +0700

    Dev tanto (#6)
    * feat: user token
    
    * feat: user auth & email verification
    
    * feat: model, migration, requests

---
+++ database/migrations/2024_03_15_093710_create_subjects_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject_name', 255);
+            $table->string('lecturer', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
